c7fd538ddfa9024b54306dd3ef7df0fd4f83142b
OE.omergunr100 <omer.er10@gmail.com>
2023-12-16 13:59:47 +0200

    Add project files.

---
+++ ShipFastLanding/Patches/ShipHubPatch.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ShipFastLanding.Patches
+{
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class ShipHubPatch
+    {
+        internal static GameObject shipHub = null;
+
+        [HarmonyPatch("SetMapScreenInfoToCurrentLevel")]
+        [HarmonyPrefix]
+        private static void speedUpShipOnCompany(StartOfRound __instance)
+        {
+            if (shipHub == null)
+            {
+                shipHub = GameObject.Find("Environment/HangarShip");
+                if (shipHub == null)
+                {
+                    Plugin.log.LogError("Didn't find ship!");
+                    return;
+                }
+                Plugin.log.LogInfo("Found ship!");
+            }
+            if (shipHub?.GetComponent<Animator>() != null)
+            {
+                if (__instance.currentLevelID == 3 || Plugin.config_fastTravelToAllPlanets.Value)
+                {
+                    Plugin.log.LogInfo("Speeding up ship!");
+                    shipHub.GetComponent<Animator>().speed = 10f;
+                }
+                else
+                {
+                    Plugin.log.LogInfo("Regular speed!");
+                    shipHub.GetComponent<Animator>().speed = 1f;
+                }
+            }
+            else
+            {
+                Plugin.log.LogError("Didn't find ship animator!");
+            }
+        }
+    }
+}
---
+++ ShipFastLanding/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace ShipFastLanding
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        private const string GUID = "oe.tweaks.qol.faster_company_landing";
+        private const string NAME = "Faster Company Landing";
+        private const string VERSION = "1.0.0";
+
+        internal static Plugin instance;
+
+        internal static BepInEx.Logging.ManualLogSource log;
+
+        internal static ConfigEntry<bool> config_fastTravelToAllPlanets; // Fast travel to all planets
+
+        private void Awake()
+        {
+            log = this.Logger;
+            log.LogInfo($"'{name}' is loading...");
+
+            if (instance == null)
+                instance = this;
+
+            config_fastTravelToAllPlanets = Config.Bind("General", "FastTravelToAll", false, "Allows you to fast travel to all planets, not just the company.");
+
+            harmony.PatchAll();
+
+            log.LogInfo($"'{NAME}' loaded!");
+        }
+    }
+}
